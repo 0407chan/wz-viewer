40a58f78a06f2cbe1565dccf602279763864125e
이찬호 <0407chan@naver.com>
2021-11-19 00:53:31 +0900

    앱 스타일 추가

--- src/appStyle.tsx
+++ src/appStyle.tsx
@@ -3,28 +3,33 @@ import styled from 'styled-components'
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  align-items: flex-start;
+  justify-content: flex-start;
   background-color: #282c34;
+  overflow: auto;
   width: 100%;
   height: 100%;
 `
 export const Header = styled.div`
   display: flex;
+  position: sticky;
+  top: 0;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-color: #282c34;
+  background-color: #414752;
+  padding: 20px;
   width: 100%;
-  height: 200px;
 `
 export const Body = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-start;
   background-color: #282c34;
+  overflow: auto;
   gap: 20px;
+  padding: 20px;
   width: 100%;
   height: 100%;
 `
@@ -33,4 +38,21 @@ export const Text = styled.span`
   color: #eeeeee;
   font-size: 18px;
   font-weight: bold;
+`
+
+export const LoadingContainer = styled.span`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #5252528c;
+  width: 100%;
+  height: 100%;
+
+  gap: 20px;
+
+  div {
+    font-size: 20px;
+    font-weight: bold;
+  }
 `
