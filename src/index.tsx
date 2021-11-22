cb21620c909ea619065bfbd2305f568ad01fc3c9
이찬호 <0407chan@naver.com>
2021-11-22 11:54:55 +0900

    router 적용

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import 'antd/dist/antd.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import './styles/globals.scss'
 const queryClient = new QueryClient()
@@ -9,7 +10,9 @@ const queryClient = new QueryClient()
 ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
